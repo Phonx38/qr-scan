c9e95f5c65f4b9543f722b0b61894ac2708c34c3
Ashutosh Tripathi <ashuuu38@gmail.com>
2021-09-10 09:25:05 +0530

    first commit

--- qr_generate/settings.py
+++ qr_generate/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0g_@^q@*e8@0mm64mh%hdk45q)sbg8ls@+l7^v9l9%mropz=+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
