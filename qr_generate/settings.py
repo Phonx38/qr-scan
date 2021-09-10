01874a2b6eb7511a1569e5925ee576f471929c72
Ashutosh Tripathi <ashuuu38@gmail.com>
2021-09-10 18:33:24 +0530

    aws

--- qr_generate/settings.py
+++ qr_generate/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-0g_@^q@*e8@0mm64mh%hdk45q)sbg8ls@+l7^v9l9%mropz="
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
